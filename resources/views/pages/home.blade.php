8825f67f21d52e1a889aebc9283d7f297b043c35
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-22 17:28:35 +0200

    added comics in main

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,8 +4,21 @@
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.css' integrity='sha512-8BU3emz11z9iF75b10oPjjpamM4Mz23yQFQymbtwyPN3mNWHxpgeqyrYnkIUP6A8KyAj5k2p3MiYLtYqew7gIw==' crossorigin='anonymous'/>
 @endsection
 
+@--dd($comics);
+
 @section("page-title", "Homepage")
 
 @section("main-content")
-    <h1>HomePage</h1>
+    <h1>Current Series</h1>
+    <section class="container">
+        <div class="row row-cols-6 justify-content-around">
+            @foreach ($comics as $comic )
+                <div class="col text-center comic-card">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" style="height: 80%">
+                        <h6> {{ $comic['series'] }} </h6>
+                </div>
+            @endforeach
+        </div>
+    </section>
+
 @endsection
