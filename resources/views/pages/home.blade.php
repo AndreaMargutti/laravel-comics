424387f24baf1281389428e05531fa6feb83e44e
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-22 17:51:00 +0200

    added blue banner

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.css' integrity='sha512-8BU3emz11z9iF75b10oPjjpamM4Mz23yQFQymbtwyPN3mNWHxpgeqyrYnkIUP6A8KyAj5k2p3MiYLtYqew7gIw==' crossorigin='anonymous'/>
 @endsection
 
-@--dd($comics);
+{{-- @dd($comics); --}}
 
 @section("page-title", "Homepage")
 
@@ -22,6 +22,31 @@
         <div class="col-12 text-center pt-5 pb-3">
             <button class="btn btn-primary">LOAD MORE</button>
         </div>
+    </section>
+    <!--Start of blue banner-->
+    <section id="blue-banner" class="d-flex align-items-center justify-content-center">
+            <ul class="d-flex align-items-center">
+                <li>
+                    <img src="{{ URL::asset('img/buy-comics-digital-comics.png')}}" alt="">
+                    DIGITAL COMICS
+                </li>
+                <li>
+                    <img src="{{ URL::asset('img/buy-comics-merchandise.png')}}" alt="">
+                    DC MERCHANDISE
+                </li>
+                <li>
+                    <img src="{{ URL::asset('img/buy-comics-subscriptions.png') }}" alt="">
+                    SUBSCRIPTION
+                </li>
+                <li>
+                    <img src="{{ URL::asset('img/buy-dc-power-visa.svg') }}" alt="">
+                    DC POWER VISA
+                </li>
+                <li>
+                    <img src="{{ URL::asset('img/buy-comics-shop-locator.png') }}" alt="">
+                    COMIC SHOP LOCATION
+                </li>
+            </ul>
     </section>
 
 @endsection
