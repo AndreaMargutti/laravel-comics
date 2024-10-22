18d89c4405f839e55a0a495b31065d475eeee862
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-22 16:22:13 +0200

    started navbar

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,9 @@
 @extends("layouts.app")
 
+@section("additional-cdn")
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.css' integrity='sha512-8BU3emz11z9iF75b10oPjjpamM4Mz23yQFQymbtwyPN3mNWHxpgeqyrYnkIUP6A8KyAj5k2p3MiYLtYqew7gIw==' crossorigin='anonymous'/>
+@endsection
+
 @section("page-title", "Homepage")
 
 @section("main-content")
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,20 @@
 <header>
-    <h2>Header</h2>
+    <nav class="d-flex align-items-center justify-content-center py-4">
+        <!--<img src="{{-- assets('img/dc-logo.png') --}}" alt="dc-logo">-->
+        <ul class="d-flex">
+            <li>Characters</li>
+            <li class="active">Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Collectibles</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+        </ul>
+        <input type="text" name="search" id="search">
+        <i class="fa-solid fa-magnifying-glass ms-3"></i>
+    </nav>
+
 </header>
