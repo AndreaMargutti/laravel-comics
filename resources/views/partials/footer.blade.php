c70dd4403bec9797a6b8db66d5259583645598aa
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-22 18:30:02 +0200

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,48 @@
 <footer>
-    <h2>Footer</h2>
+    <footer class="row">
+        <div class="col-8 d-flex justify-content-center pt-4">
+            <ul>
+                <li><h4>DC Comics</h4></li>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+            <ul>
+                <li><h4>DC</h4></li>
+                <li>Terms of Use</li>
+                <li>Privacy Policy</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshop</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+            <ul>
+                <li><h4>SITES</h4></li>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+            </ul>
+            <ul>
+                <li>
+                    <h4>SHOP</h4>
+                </li>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+            </ul>
+        </div>
+        <div class="col-4">
+            <img src="{{ URL::asset('img/dc-logo-bg.png') }}" alt="">
+        </div>
+    </footer>
 </footer>
